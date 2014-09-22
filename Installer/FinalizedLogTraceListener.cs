dd863822db1caf504ddb419baa6b9fc25a817ae8
scorpion <scorpion.jze@gmail.com>
2014-09-21 21:56:42 -0500

    FinalizedLogTraceListener now works as intended

--- Installer/FinalizedLogTraceListener.cs
+++ Installer/FinalizedLogTraceListener.cs
@@ -4,22 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Diagnostics;
 using ICSharpCode.SharpZipLib.Checksums;
+using System.IO;
 
 namespace TaleOfTwoWastelands
 {
     class FinalizedLogTraceListener : TextWriterTraceListener
     {
         readonly Crc32 _checksum = new Crc32();
+        readonly Stream _logStream;
 
         public FinalizedLogTraceListener(string path)
-            : base(path)
+            : this(File.OpenWrite(path))
         {
+        }
+        FinalizedLogTraceListener(Stream stream)
+            : base(stream)
+        {
+            _logStream = stream;
+            //TraceOutputOptions |= TraceOptions.DateTime | TraceOptions.Callstack;
         }
 
         private void UpdateChecksum(string s)
         {
             var messageBytes = Writer.Encoding.GetBytes(s);
             _checksum.Update(messageBytes);
+        }
+
+        private void WriteChecksum()
+        {
+            base.Write(_checksum.Value.ToString("X16"));
+        }
+
+        private void GoBack()
+        {
+            if (_logStream.Position > 16)
+                _logStream.Seek(-16, SeekOrigin.Current);
+        }
+
+        private string Timestamp(string msg)
+        {
+            return string.Format("[{0:u}] {1}", DateTime.UtcNow, msg);
         }
 
         public override bool IsThreadSafe
@@ -30,32 +54,13 @@ namespace TaleOfTwoWastelands
             }
         }
 
-        public override void Write(string message)
-        {
-            UpdateChecksum(message);
-            base.Write(message);
-        }
-
         public override void WriteLine(string message)
         {
+            message = Timestamp(message);
             UpdateChecksum(message);
+            GoBack();
             base.WriteLine(message);
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            try
-            {
-                if (disposing)
-                {
-                    Flush();
-                    base.WriteLine(_checksum.Value.ToString("X"));
-                }
-            }
-            finally
-            {
-                base.Dispose(disposing);
-            }
+            WriteChecksum();
         }
     }
 }
