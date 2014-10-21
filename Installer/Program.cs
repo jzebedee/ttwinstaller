75f630f28680997a584c7857bc786ddc6c200a92
scorpion <scorpion.jze@gmail.com>
2014-10-21 04:34:57 -0500

    Removed reference to FinalizedLogTraceListener

--- Installer/Program.cs
+++ Installer/Program.cs
@@ -34,7 +34,6 @@ namespace TaleOfTwoWastelands
             var logFilepath = Path.Combine(LogDirectory, logFilename);
 
             Trace.AutoFlush = true;
-            Trace.Listeners.Add(new FinalizedLogTraceListener(logFilepath));
         }
 
         static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
