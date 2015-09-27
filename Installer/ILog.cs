48a20031fc9d3baf4b442cf06be3d014d92909f7
J. Zebedee <zebedee@code.gripe>
2015-09-27 10:30:00 -0500

    Retarget projects to .NET 4.5.2
    - Update nuget packages to latest
    - Replace Progress<T> implementation with the BCL version

--- Installer/ILog.cs
+++ Installer/ILog.cs
@@ -1,11 +1,11 @@
-﻿using TaleOfTwoWastelands.Progress;
+﻿using System;
 
 namespace TaleOfTwoWastelands
 {
     public interface ILog
     {
         IProgress<string> DisplayMessage { get; set; }
-        
+
         void File(string msg, params object[] args);
         void Display(string msg, params object[] args);
         void Dual(string msg, params object[] args);
