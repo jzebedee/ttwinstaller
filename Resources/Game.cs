7023485f079bbda86c4e75f43080a652b2370d55
J. Zebedee <zebedee@code.gripe>
2015-09-27 10:12:16 -0500

    Fix references in Resources.Game

--- Resources/Game.cs
+++ Resources/Game.cs
@@ -8,9 +8,9 @@ namespace Resources
 
     public static class Game
     {
-        public static readonly IDSSA CheckedBSAs = JsonConvert.DeserializeObject<IDSSA>(Properties.Resources.CheckedBSAs);
-        public static readonly IDSS BuildableBSAs = JsonConvert.DeserializeObject<IDSS>(Properties.Resources.BuildableBSAs);
-        public static readonly IDSS VoicePaths = JsonConvert.DeserializeObject<IDSS>(Properties.Resources.VoicePaths);
-        public static readonly string[] CheckedESMs = JsonConvert.DeserializeObject<string[]>(Properties.Resources.CheckedESMs);
+        public static readonly IDSSA CheckedBSAs = JsonConvert.DeserializeObject<IDSSA>(TaleOfTwoWastelands.Properties.Resources.CheckedBSAs);
+        public static readonly IDSS BuildableBSAs = JsonConvert.DeserializeObject<IDSS>(TaleOfTwoWastelands.Properties.Resources.BuildableBSAs);
+        public static readonly IDSS VoicePaths = JsonConvert.DeserializeObject<IDSS>(TaleOfTwoWastelands.Properties.Resources.VoicePaths);
+        public static readonly string[] CheckedESMs = JsonConvert.DeserializeObject<string[]>(TaleOfTwoWastelands.Properties.Resources.CheckedESMs);
     }
 }
